dca3364251f5c0b31f563c70202c691e80e2b1d2
MatheusFortes7 <mfortesv@gmail.com>
2023-06-21 15:56:45 -0300

    first day of code

---
+++ react-project/src/Hooks/useForm.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const validationTypes = {
+  email: {
+    regex: /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i,
+    message: "Preencha um email válido",
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState("");
+  const [error, setError] = React.useState(null);
+
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError("Campo Obrigatório");
+      return false;
+    } else if (validationTypes[type] && !validationTypes[type].regex.test(value)) {
+      setError(validationTypes[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  function onChange({ target }) {
+    if(error) validate(target.value);
+    setValue(target.value);
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
